1b427c428205dab78918c5f152a1eea3a2dfbfe5
MinHien-git <64275316+MinHien-git@users.noreply.github.com>
2023-10-06 15:55:58 +0700

    first commit

---
+++ constants/user.type.js
@@ -0,0 +1,6 @@
+let USER_TYPE = {
+  Ward_Officials,
+  Street_Officials,
+  Department_Officers,
+  Citizens,
+};
---
+++ models/users.model.js
@@ -0,0 +1,19 @@
+class User {
+  constructor(email, password, phone, name, birth) {
+    this.email = email;
+    this.password = password;
+    this.name = name;
+    this.birth = birth;
+    this.phone = phone;
+  }
+
+  _login() {}
+
+  _register() {}
+
+  _update() {}
+
+  _change_password() {}
+
+  _forgot_password() {}
+}
