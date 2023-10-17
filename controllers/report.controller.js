b71cd1f5c0d55e4f11a8612fa4ac1bf1d0a934f3
MinHien-git <64275316+MinHien-git@users.noreply.github.com>
2023-10-17 18:52:25 +0700

    add report

---
+++ controllers/report.controller.js
@@ -0,0 +1,7 @@
+const _get_report = (request, response) => {};
+
+const _post_report = (request, response) => {};
+
+const _update_report_state = (request, response) => {};
+
+module.exports = { _get_report, _post_report, _update_report_state };
