b205c945595fa1676562786812b142a858a4304b
duck420i69 <88267139+duck420i69@users.noreply.github.com>
2023-10-18 17:11:51 +0700

    add report

---
+++ models/report.model.js
@@ -0,0 +1,13 @@
+class Report {
+    constructor(
+        billboard_id,
+        report_title,
+        report_content,
+        reporter_email
+    ) {
+        this.billboard_id = billboard_id
+        this.report_title = report_title
+        this.report_content = report_content
+        this.reporter_email = reporter_email
+    }
+}
