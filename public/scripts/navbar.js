d2d1f12a51239fdeb5644092706eb8edb53db3b9
MinHien <64275316+MinHien-git@users.noreply.github.com>
2023-11-28 17:54:55 +0700

    update nav_bar

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,24 +1,24 @@
 let primary_header = $("#primary-header");
 let burger = $("#burger");
-let logout = $("#logout button")
+// let logout = $("#logout button");
 let primary_navigation_bar = $("#primary-navigation-bar");
 
 burger.on("click", (e) => {
   primary_header.toggleClass("active");
 });
 
-logout.on("click", (e) => {
-    e.preventDefault();
-    window.location.href = "/Phan-cum-nguoi-dan/trangchu.html"
-})
+// logout.on("click", (e) => {
+//   e.preventDefault();
+//   window.location.href = "/";
+// });
 
 let navbar = document.querySelectorAll("#primary-items-list li");
 navbar.forEach((e) => {
-    e.addEventListener("mouseover", () => {
-        e.style.transition = "color 50ms ease-in-out";
-        e.style.color = "hsl(219, 90%, 69%)";
-    });
-    e.addEventListener("mouseout", () => {
-        e.style.color = "black";
-    })
-})
+  e.addEventListener("mouseover", () => {
+    e.style.transition = "color 50ms ease-in-out";
+    e.style.color = "hsl(219, 90%, 69%)";
+  });
+  e.addEventListener("mouseout", () => {
+    e.style.color = "black";
+  });
+});
--- utils/authentication.js
+++ utils/authentication.js
@@ -3,6 +3,7 @@ const create_user_session = (request, user, action) => {
   request.session.email = user.email;
   request.session.ward = user.ward;
   request.session.street = user.street;
+  request.session.type_user = user.type_user ? user.type_user : 0;
   request.session.save(action);
 };
 
@@ -11,6 +12,7 @@ const delete_user_authentication = (request) => {
   request.session.email = null;
   request.session.ward = null;
   request.session.street = null;
+  request.session.type_user = 0;
 };
 module.exports = {
   create_user_session,
