4077dc32ee4f384a6afb0dade6b130787cfd4ad0
Huy <mana27112002@gmail.com>
2023-12-09 00:11:02 +0700

    change home route for department user

--- routes/home.route.js
+++ routes/home.route.js
@@ -4,5 +4,7 @@ const map_controller = require("../controllers/map.controller");
 const router = express.Router();
 
 router.get("/", map_controller._get_map);
-router.get("/management", map_controller._manage_map);
+router.get("/management", (req, res) => {
+    res.redirect("management/billboards");
+});
 module.exports = router;
